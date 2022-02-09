e623fc775ebdeb36acf6072157288962be4dd83f
prudhvich957 <53164767+prudhvich957@users.noreply.github.com>
2022-02-09 10:57:23 +0530

    minor update

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
   googleClientID:
-    '896525380332-vog4aodf6lf0i26fa3bhnnc635mdoqn2.apps.googleusercontent.com',
-  googleClientSecret: 'iR80b2MIaEkevs20e_QdBCN0',
-  mongoURI: 'mongodb+srv://prudhvi:0$yVL4U3!@cluster0.i8eba.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    '',
+  googleClientSecret: '',
+  mongoURI: '',
 //  mongodb+srv://prudhvi:<password>@cluster0.i8eba.mongodb.net/<dbname>?retryWrites=true&w=majority
-  cookieKey: '123123123',
+  cookieKey: '',
   redisUrl: 'redis://127.0.0.1:6379',
   accessKeyId: '',
   secretAccessKey: ''
